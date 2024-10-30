0b297b58340ee114ea26d1a3cdae76509a4bd6dc
varun252k4 <148788427+varun252k4@users.noreply.github.com>
2024-10-30 19:42:38 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -167,3 +167,7 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    def test_get_account_not_found(self):
+      """It should not Read an Account that is not found"""
+      resp = self.client.get(f"{BASE_URL}/0")
+      self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
